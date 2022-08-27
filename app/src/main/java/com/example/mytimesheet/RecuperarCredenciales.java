bf4af34fd07577a6f3d67d5cc3b466fd5703b48c
daveloper1978 <david.pardof@gmail.com>
2022-08-26 23:50:48 -0500

    Versión base 1

--- app/src/main/java/com/example/mytimesheet/RecuperarCredenciales.java
+++ app/src/main/java/com/example/mytimesheet/RecuperarCredenciales.java
@@ -2,7 +2,9 @@ package com.example.mytimesheet;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class RecuperarCredenciales extends AppCompatActivity {
 
@@ -10,5 +12,11 @@ public class RecuperarCredenciales extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_recuperar_credenciales);
+    }
+
+    public void enviar(View v){
+
+        startActivity(new Intent(this, MainActivity.class));
+
     }
 }
